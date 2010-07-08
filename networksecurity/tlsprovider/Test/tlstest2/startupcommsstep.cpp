516af977c635a415211c3210509bbc9edfb87099
hgs <none@none>
2010-07-08 15:21:49 +0530

    201027_01

--- networksecurity/tlsprovider/Test/tlstest2/startupcommsstep.cpp
+++ networksecurity/tlsprovider/Test/tlstest2/startupcommsstep.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2008-2009 Nokia Corporation and/or its subsidiary(-ies).
+// Copyright (c) 2008-2010 Nokia Corporation and/or its subsidiary(-ies).
 // All rights reserved.
 // This component and the accompanying materials are made available
 // under the terms of "Eclipse Public License v1.0"
@@ -37,6 +37,7 @@ TVerdict CStartupCommsStep::doTestStepL()
 	{
 	SetTestStepResult(EFail);
 	
+#ifndef SIROCCO_CODE_MIGRATION
 	TInt ret = User::LoadPhysicalDevice(PDD_NAME);
 	User::LeaveIfError(ret == KErrAlreadyExists?KErrNone:ret);
 
@@ -50,7 +51,7 @@ TVerdict CStartupCommsStep::doTestStepL()
 	User::LeaveIfError(ret == KErrAlreadyExists?KErrNone:ret);
 	ret = StartC32();
 	User::LeaveIfError(ret == KErrAlreadyExists?KErrNone:ret);
+#endif //SIROCCO_CODE_MIGRATION
 	SetTestStepResult(EPass);
-
 	return TestStepResult();
 	}
